bb9426f696c6d22ceaefc1c8242db72f4d5b9849
Sora's Playground <kayozzprojects@yandex.com>
2024-09-11 02:41:34 +0800

    Create twrp_KJ6.mk
    Hello twrp kj6

---
+++ twrp_KJ6.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from KJ6 device
+$(call inherit-product, device/tecno/KJ6/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_KJ6
+PRODUCT_DEVICE := KJ6
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Tecno KJ6
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
