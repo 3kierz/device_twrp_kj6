b118ac0527d121c3719df1988ada34be2c1f1bf5
Sora's Playground <kayozzprojects@yandex.com>
2024-09-11 02:43:27 +0800

    Create BoardConfig.mk

---
+++ BoardConfig.mk
@@ -0,0 +1,16 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/tecno/KJ6
+
+# Inherit from mt6789-common
+include device/transsion/mt6789-common/BoardConfigCommon.mk
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := Tecno-KJ6
+
+# TWRP Configs
+TW_DEVICE_VERSION := KJ6_by_rama982_sora1504
